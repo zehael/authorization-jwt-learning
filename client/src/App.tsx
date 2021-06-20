8d86c893e2cc80b66a5d3feeeecef2a9086181ee
zhalil <evg29227704@gmail.com>
2021-06-20 15:17:50 +0500

    updated git settings

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// Test
+console.log("git test");
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
